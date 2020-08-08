ce049ad52c1f4fc13c45c534c0fc7951bbcf2675
Luciano2018 <lucianorabassa@gmail.com>
2020-08-07 23:47:33 -0300

    version 0

--- instalar.sh
+++ instalar.sh
@@ -5,29 +5,60 @@
 # License: http://creativecommons.org/licenses/by-sa/4.0/
 ###########################################################################
 echo "Moviendo las Bios al lugar adecuado"
-cp /home/pi/RetroPieBios/BIOS/* /home/pi/RetroPie/BIOS
+cp -r /home/pi/RetroPieBios/BIOS/* /home/pi/RetroPie/BIOS/
 echo
 echo "BIOS movidas"
-echo "moviendo BIOS especiales"
-sudo mkdir /opt/retropie/configs/nds/
-sudo mkdir /opt/retropie/configs/nds/drastic/
-sudo mkdir /opt/retropie/configs/nds/drastic/system/
-sudo cp /home/pi/RetroPie/RetroPieBios/opt/retropie/configs/nds/drastic/system/* /opt/retropie/configs/nds/drastic/system/
-sudo cp /home/pi/RetroPie/RetroPieBios/opt/retropie/emulators/retroarch/bin/* /opt/retropie/emulators/retroarch/bin/
 echo
+echo "moviendo BIOS especiales"
+echo
+echo "Creando carpetas inexistentes"
+sudo mkdir /opt/retropie/configs/nds/
+echo "Carpeta nds creada"
+sudo mkdir /opt/retropie/configs/nds/drastic/
+echo "Carpeta drastic creada"
+sudo mkdir /opt/retropie/configs/nds/drastic/system/
+echo "Carpeta system creada"
+sudo cp -r /home/pi/RetroPieBios/opt/retropie/configs/nds/drastic/system* /opt/retropie/configs/nds/drastic/system/
+echo "Bios Drastic copiadas"
+sudo cp -r /home/pi/RetroPieBios/opt/retropie/emulators/retroarch/bin* /opt/retropie/emulators/retroarch/bin/
+echo "Bios Macintosh copiada"
 echo "Moviendo BIOS que deben estar en la carpeta roms"
+echo "Creando carpeta macintosh"
 mkdir /home/pi/RetroPie/roms/macintosh/
-cp /home/pi/RetroPie/RetroPieBios/roms/macintosh/* /home/pi/RetroPie/roms/macintosh/
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/neogeo/
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/arcade/
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/fba/
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-libretro/
+echo "Carpeta macintosh creada"
+cp -r /home/pi/RetroPieBios/roms/macintosh/* /home/pi/RetroPie/roms/macintosh/
+echo "Bios macintosh copiada"
+echo
+echo "Copiando Bios neogeo a las carpetas correspondientes" 
+echo
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/neogeo/
+echo "Bios copiada en directorio neogeo"
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/arcade/
+echo "Bios copiada en directorio arcade"
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/fba/
+echo "Bios copiada en directorio fba"
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-libretro/
+echo "Bios copiada en directorio mame-libretro"
+echo
+echo "Creando directorio mame-mame4all"
 mkdir /home/pi/RetroPie/roms/mame-mame4all
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-mame4all/
+echo "Creado"
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-mame4all/
+echo "Bios copiada en directorio mame-mame4all"
+echo
+echo "Creando directorio mame-advmame"
 mkdir /home/pi/RetroPie/roms/mame-advmame
-cp /home/pi/RetroPie/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-advmame/
-echo "Copiadas todas las BIOS"
+echo "Creado"
+cp -r /home/pi/RetroPieBios/roms/neogeo/* /home/pi/RetroPie/roms/mame-advmame/
+echo "Bios copiada en directorio mame-advmame"
+echo
+echo "Al fin, Copiadas todas las BIOS"
+echo
+echo "Regresando a cas para pasar la escoba"
 cd
 sudo rm -rf /home/pi/RetroPieBios/
+echo "Listo, todo limpio"
 echo "Reinicia tu Raspberry Pi con <sudo reboot> y Disfruta"
-echo "Raspberry Pi Buenos Aires"
+echo "Saludos desde Raspberry Pi Buenos Aires"
+echo
+echo
